022ba59aaa47d9c1a8f9e5aefcae2be4da163e27
leandroagustinmena <leandromena_94@hotmail.com>
2022-02-09 14:41:21 -0300

    5do intento

--- db.js
+++ db.js
@@ -1,46 +1,46 @@
-const knex = require("knex")({
-  client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    port: 3307,
-    user: "root",
-    database: "ecommerce",
-  },
-  pool: { min: 2, max: 8 },
-});
+// const knex = require("knex")({
+//   client: "mysql",
+//   connection: {
+//     host: "127.0.0.1",
+//     port: 3307,
+//     user: "root",
+//     database: "ecommerce",
+//   },
+//   pool: { min: 2, max: 8 },
+// });
 
-//Id, Nombre, Descripcion, codigo, foto, precio, stock, timestamp, id
-knex.schema
-  .hasTable("products", (table) => {
-    table.increments("id").primary(),
-      table.timestamp("created_at").defaultTo(knex.fn.now()),
-      table.string("nombre"),
-      table.string("descripcion"),
-      table.string("codigo"),
-      table.string("foto"),
-      table.float("precio"),
-      table.integer("stock");
-  })
-  .then(() => {
-    console.log("Tabla creada con exito");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// //Id, Nombre, Descripcion, codigo, foto, precio, stock, timestamp, id
+// knex.schema
+//   .hasTable("products", (table) => {
+//     table.increments("id").primary(),
+//       table.timestamp("created_at").defaultTo(knex.fn.now()),
+//       table.string("nombre"),
+//       table.string("descripcion"),
+//       table.string("codigo"),
+//       table.string("foto"),
+//       table.float("precio"),
+//       table.integer("stock");
+//   })
+//   .then(() => {
+//     console.log("Tabla creada con exito");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-knex.schema
-  .hasTable("messages", (table) => {
-    table.increments("id").primary(),
-      table.timestamp("created_at").defaultTo(knex.fn.now()),
-      table.string("nombre"),
-      table.string("mensaje");
-  })
-  .then(() => {
-    console.log("Tabla creada con exito");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// knex.schema
+//   .hasTable("messages", (table) => {
+//     table.increments("id").primary(),
+//       table.timestamp("created_at").defaultTo(knex.fn.now()),
+//       table.string("nombre"),
+//       table.string("mensaje");
+//   })
+//   .then(() => {
+//     console.log("Tabla creada con exito");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
